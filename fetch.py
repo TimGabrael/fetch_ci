afa7a137b9932ba6e687d6af5ba09d7ebf56176e
Maiko D <dederichs.maiko@gmail.com>
2025-06-01 16:35:56 +0200

    update fetch.py

--- fetch.py
+++ fetch.py
@@ -27,3 +27,15 @@ response = requests.get(url);
 if response.status_code == 200:
     with open(file_path, 'w', encoding='utf-8') as f:
         f.write(response.text)
+
+url = "https://www.nytimes.com/svc/crosswords/v6/puzzle/mini.json"
+folder = "nyt"
+
+os.makedirs(folder, exist_ok=True)
+file_path = os.path.join(folder, f"{current_date}.json");
+
+response = requests.get(url);
+
+if response.status_code == 200:
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(response.text)
