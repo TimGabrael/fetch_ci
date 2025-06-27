af7604d9287cc189f3de9b978a97d4e8f99e9ba7
TimGabrael <75118988+TimGabrael@users.noreply.github.com>
2025-06-27 14:53:55 +0200

    added header

--- fetch.py
+++ fetch.py
@@ -3,13 +3,17 @@ import requests
 import os
 current_date = datetime.now().strftime("%Y-%m-%d")
 
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
+}
+
 url = f"https://spiele.spiegel.de/games/spiegel_Crosswords/js/data/levels/c003/{current_date}_9x9.json?_=1.14.112"
 folder = "spiegel"
 
 os.makedirs(folder, exist_ok=True)
 file_path = os.path.join(folder, f"{current_date}.json");
 
-response = requests.get(url);
+response = requests.get(url, headers=headers);
 
 if response.status_code == 200:
     with open(file_path, 'w', encoding='utf-8') as f:
@@ -22,7 +26,7 @@ folder = "sueddeutsche"
 os.makedirs(folder, exist_ok=True)
 file_path = os.path.join(folder, f"{current_date}.html");
 
-response = requests.get(url);
+response = requests.get(url, headers=headers);
 
 if response.status_code == 200:
     with open(file_path, 'w', encoding='utf-8') as f:
@@ -34,7 +38,7 @@ folder = "nyt"
 os.makedirs(folder, exist_ok=True)
 file_path = os.path.join(folder, f"{current_date}.json");
 
-response = requests.get(url);
+response = requests.get(url, headers=headers);
 
 if response.status_code == 200:
     with open(file_path, 'w', encoding='utf-8') as f:
