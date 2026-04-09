7a18c112fb90b098b0d879b39db9159373b6b498
Maiko D <dederichs.maiko@gmail.com>
2026-04-09 14:23:24 +0200

    ...

--- fetch.py
+++ fetch.py
@@ -4,20 +4,19 @@ import os
 current_date = datetime.now().strftime("%Y-%m-%d")
 
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36'
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36',
+    'x-games-auth-bypass': 'true'
 }
 
-url = f"https://spiele.spiegel.de/games/spiegel_Crosswords/js/data/levels/c003/{current_date}_9x9.json?_=1.14.112"
-folder = "spiegel"
-
-os.makedirs(folder, exist_ok=True)
-file_path = os.path.join(folder, f"{current_date}.json");
-
-response = requests.get(url, headers=headers);
-
-if response.status_code == 200:
-    with open(file_path, 'w', encoding='utf-8') as f:
-        f.write(response.text)
+# Spiegel changed their system. Maybe add it back later, but who cares
+#url = f"https://spiele.spiegel.de/games/spiegel_Crosswords/js/data/levels/c003/{current_date}_9x9.json?_=1.14.112"
+#folder = "spiegel"
+#os.makedirs(folder, exist_ok=True)
+#file_path = os.path.join(folder, f"{current_date}.json");
+#response = requests.get(url, headers=headers);
+#if response.status_code == 200:
+#    with open(file_path, 'w', encoding='utf-8') as f:
+#        f.write(response.text)
 
 suedeutsche_datetime = datetime.now().strftime("%Y%m%d");
 url = f"https://www.sueddeutsche.de/tools/spiele/kreuzwortraetsel/iframe?currentDate={suedeutsche_datetime}"
